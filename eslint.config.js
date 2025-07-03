0b0b64a5dce1a1d27cdc7228efc0aee2f0ce01f8
Mxm Msv <oikioikioikiabrams@gmail.com>
2025-07-03 14:09:40 +0300

    chore: install import sorter

--- eslint.config.js
+++ eslint.config.js
@@ -6,15 +6,25 @@ import react from 'eslint-plugin-react';
 import tseslint from 'typescript-eslint';
 import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
 import reactCompiler from 'eslint-plugin-react-compiler';
+import importPlugin from 'eslint-plugin-import';
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.strict, eslintPluginPrettier],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.strict,
+      eslintPluginPrettier,
+      importPlugin.flatConfigs.recommended,
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
     plugins: {
       react,
@@ -23,16 +33,37 @@ export default tseslint.config(
       'react-compiler': reactCompiler,
     },
     rules: {
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
       'react-compiler/react-compiler': 'error',
-      ...react.configs.recommended.rules,
-      ...react.configs['jsx-runtime'].rules,
+      'import/order': [
+        'error',
+        {
+          groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+          'newlines-between': 'always',
+          pathGroups: [
+            {
+              pattern: '@/**',
+              group: 'internal',
+            },
+          ],
+          alphabetize: {
+            order: 'asc',
+          },
+        },
+      ],
     },
+  },
+  {
     settings: {
       react: {
         version: 'detect',
       },
+      'import/resolver': {
+        typescript: true,
+      },
     },
-  },
+  }
 );
--- prettier.config.js
+++ prettier.config.js
@@ -2,9 +2,11 @@ export default {
   semi: true,
   singleQuote: true,
   tabWidth: 2,
-  trailingComma: 'all',
-  printWidth: 100,
+  trailingComma: 'es5',
+  printWidth: 120,
   jsxSingleQuote: false,
   bracketSameLine: false,
+  arrowParens: 'always',
+  endOfLine: 'lf',
   plugins: ['prettier-plugin-tailwindcss'],
 };
