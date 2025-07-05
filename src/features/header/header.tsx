c6cc52b3e4c52bf9b1dbe1c9b6c36266b36face4
Mxm Msv <oikioikioikiabrams@gmail.com>
2025-07-05 22:44:05 +0300

    feat: add header

--- src/features/header/header.tsx
+++ src/features/header/header.tsx
@@ -1,10 +1,21 @@
 import { Component } from 'react';
 
+import SearchBar from '@/features/search/search-bar';
+
 export default class Header extends Component {
   render() {
     return (
       <header>
-        <span className="flex border-2 text-3xl">hello world</span>
+        <div className="">
+          <img
+            src="./Rick_and_Morty.svg"
+            alt="Rick and Morty logo"
+            loading="lazy"
+            decoding="async"
+            className="h-full w-full [filter:drop-shadow(0_0_0_var(--color-chart-3))] transition duration-300 hover:[filter:drop-shadow(0_0_1rem_var(--color-chart-3))]"
+          />
+        </div>
+        <SearchBar />
       </header>
     );
   }
