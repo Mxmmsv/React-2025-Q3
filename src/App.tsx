f379b9ade556e102daed0aa99672c5aa1bd19696
Mxm Msv <oikioikioikiabrams@gmail.com>
2025-07-06 02:32:19 +0300

    feat: add main to the app and add some styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Header from '@/features/header/header';
+import Main from '@/features/main/main';
 
 function App() {
   return (
-    <>
+    <div className="m-5 flex min-h-screen flex-col gap-5">
       <Header></Header>
-    </>
+      <Main></Main>
+    </div>
   );
 }
 
