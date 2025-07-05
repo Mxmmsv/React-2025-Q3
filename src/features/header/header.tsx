7570ef1c84f41c5fd894b0b587644178cb6d7e57
Mxm Msv <oikioikioikiabrams@gmail.com>
2025-07-06 02:32:53 +0300

    style: add some style for header

--- src/features/header/header.tsx
+++ src/features/header/header.tsx
@@ -5,14 +5,14 @@ import SearchBar from '@/features/search/search-bar';
 export default class Header extends Component {
   render() {
     return (
-      <header>
-        <div className="">
+      <header className="flex flex-row items-center justify-between">
+        <div>
           <img
             src="./Rick_and_Morty.svg"
             alt="Rick and Morty logo"
             loading="lazy"
             decoding="async"
-            className="h-full w-full [filter:drop-shadow(0_0_0_var(--color-chart-3))] transition duration-300 hover:[filter:drop-shadow(0_0_1rem_var(--color-chart-3))]"
+            className="h-16 w-full [filter:drop-shadow(0_0_0_var(--color-muted-hero))] transition duration-300 hover:[filter:drop-shadow(0_0_1rem_var(--color-muted-hero))]"
           />
         </div>
         <SearchBar />
