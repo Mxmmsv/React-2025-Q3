f8a334f96fe1684316f52163196308bc703bd13c
Mxm Msv <oikioikioikiabrams@gmail.com>
2025-07-06 15:51:02 +0300

    feat: add save to localstorage input value

--- src/features/header/search-bar.tsx
+++ src/features/header/search-bar.tsx
@@ -1,15 +1,37 @@
-import { Component } from 'react';
+import { Component, type ChangeEvent, type FormEvent } from 'react';
 
 import Button from '@/components/button';
 import Input from '@/components/input';
 
 class SearchBar extends Component {
+  state = {
+    inputValue: localStorage.getItem('INPUT-VALUE') || '',
+  };
+
+  handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    this.setState({ inputValue: e.target.value });
+  };
+
+  handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    localStorage.setItem('INPUT-VALUE', this.state.inputValue);
+    return console.log('input value:', this.state.inputValue);
+  };
   render() {
     return (
-      <div className="flex w-full flex-row gap-10">
-        <Input placeholder="please write smth" id="" name="" className="bg-background"></Input>
-        <Button>Submit</Button>
-      </div>
+      <form onSubmit={(e) => void this.handleSubmit(e)} className="w-full">
+        <div className="flex w-full flex-row gap-10">
+          <Input
+            placeholder="please write smth"
+            id=""
+            name=""
+            className="bg-background"
+            value={this.state.inputValue}
+            onChange={this.handleInputChange}
+          ></Input>
+          <Button type="submit">Submit</Button>
+        </div>
+      </form>
     );
   }
 }
