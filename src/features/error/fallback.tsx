d25378b7ad8a0b29ead08883f98026d0a61a4438
Mxm Msv <oikioikioikiabrams@gmail.com>
2025-07-11 04:11:42 +0300

    feat: add style for fallback

--- src/features/error/fallback.tsx
+++ src/features/error/fallback.tsx
@@ -1,22 +1,44 @@
 import { Component } from 'react';
 
 import Button from '@/components/button';
-import { cn } from '@/utils/cn';
+import HttpStatusCode from '@/utils/http-status-code';
 
 import type { ErrorFallbackProps } from './types';
 
 class ErrorFallback extends Component<ErrorFallbackProps> {
   render() {
-    const { onReset, error, errorInfo } = this.props;
+    const { onReset, error } = this.props;
     return (
-      <div className={cn('flex flex-col items-center justify-center gap-10', 'rounded-xl')}>
+      <div
+        className={
+          'bg-main-background flex min-h-screen flex-col items-center justify-center gap-10 bg-repeat p-5'
+        }
+        style={{
+          backgroundImage: `url('./background/rick-head.svg'), url('./background/morty-head.svg')`,
+          backgroundPosition: '0 0, 50px 50px',
+        }}
+      >
+        <div>
+          <img
+            src="./Rick_and_Morty.svg"
+            alt="Rick and Morty logo"
+            loading="lazy"
+            decoding="async"
+            className="h-32 w-full [filter:drop-shadow(0_0_0_var(--color-muted-hero))] transition duration-300 hover:[filter:drop-shadow(0_0_1rem_var(--color-muted-hero))]"
+          />
+        </div>
+        <div className="border-muted-hero bg-background text-destructive w-full overflow-x-auto rounded-md border p-4 text-left text-xl whitespace-pre-wrap">
+          {error.message === HttpStatusCode.NOT_FOUND.toString() && (
+            <>
+              <p>Error 404</p>
+              <p>please write correct query</p>
+              <p>Your current query: {localStorage.getItem('INPUT-VALUE')}</p>
+            </>
+          )}
+        </div>
         <Button onClick={onReset} className="w-full">
           Reset
         </Button>
-        <div className="border-muted-hero bg-background text-destructive w-full overflow-x-auto rounded-md border p-4 text-left text-sm whitespace-pre-wrap">
-          <p>{error.message}</p>
-          <p>{errorInfo.componentStack}</p>
-        </div>
       </div>
     );
   }
