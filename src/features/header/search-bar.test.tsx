ff4926f04d987c150b6480ea1dc2e853ef9721fd
Mxm Msv <oikioikioikiabrams@gmail.com>
2025-07-17 00:37:11 +0300

    test: add user interaction with input and button tests for searchbar component

--- src/features/header/search-bar.test.tsx
+++ src/features/header/search-bar.test.tsx
@@ -1,20 +1,65 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { describe, it, expect, beforeEach } from 'vitest';
 
 import SearchBar from './search-bar';
 
 describe('Search bar', () => {
   beforeEach(() => {
-    render(<SearchBar onSearch={() => {}} />);
+    localStorage.clear();
   });
 
-  it('should render search input', () => {
-    const input = screen.getByPlaceholderText(/please write smth/i);
-    expect(input).toBeInTheDocument();
+  describe('Render tests', () => {
+    it('Should render search input', () => {
+      render(<SearchBar onSearch={() => {}} />);
+      const input = screen.getByPlaceholderText(/please write smth/i);
+
+      expect(input).toBeInTheDocument();
+    });
+
+    it('Should render submit button', () => {
+      render(<SearchBar onSearch={() => {}} />);
+      const button = screen.getByRole('button');
+
+      expect(button).toBeInTheDocument();
+    });
+
+    it('Should display an empty search query on mount', () => {
+      render(<SearchBar onSearch={() => {}} />);
+      const input = screen.getByPlaceholderText(/please write smth/i);
+
+      expect(input).toHaveValue('');
+    });
+
+    it('Should display previously saved search query from localStorage on mount', () => {
+      localStorage.setItem('INPUT-VALUE', 'Hello World!');
+      render(<SearchBar onSearch={() => {}} />);
+      const input = screen.getByPlaceholderText(/please write smth/i);
+
+      expect(input).toHaveValue('Hello World!');
+    });
   });
 
-  it('should render submit button', () => {
-    const button = screen.getByRole('button');
-    expect(button).toBeInTheDocument();
+  describe('User interaction tests', () => {
+    it('Should render previous search query', async () => {
+      const user = userEvent.setup();
+      const component = render(<SearchBar onSearch={() => {}} />);
+
+      const input = screen.getByPlaceholderText(/please write smth/i);
+      const button = screen.getByRole('button');
+
+      await user.click(input);
+      await user.type(input, 'Hello World!');
+      await user.click(button);
+
+      expect(localStorage.getItem('INPUT-VALUE')).toBe('Hello World!');
+
+      component.unmount();
+
+      render(<SearchBar onSearch={() => {}} />);
+      const newInput = screen.getByPlaceholderText(/please write smth/i);
+
+      expect(newInput).toHaveValue('Hello World!');
+    });
   });
 });
