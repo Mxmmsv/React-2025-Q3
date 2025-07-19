d03dbb701c18864841f3ca60d0c2dc54c16dcc4d
Mxm Msv <oikioikioikiabrams@gmail.com>
2025-07-19 22:32:31 +0300

    refactor: change class component to functional component for app.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,28 +1,24 @@
-import { Component } from 'react';
-
 import ErrorBoundary from '@/features/error/error-boundary';
 import ErrorFallback from '@/features/error/fallback';
 
 import Wrapper from './wrapper';
 
-class App extends Component {
-  render() {
-    return (
-      <ErrorBoundary
-        fallback={(error, handleReset) => <ErrorFallback onReset={handleReset} error={error} />}
+function App() {
+  return (
+    <ErrorBoundary
+      fallback={(error, handleReset) => <ErrorFallback onReset={handleReset} error={error} />}
+    >
+      <div
+        className="bg-main-background flex min-h-screen flex-col gap-5 bg-repeat p-5"
+        style={{
+          backgroundImage: `url('./background/rick-head.svg'), url('./background/morty-head.svg')`,
+          backgroundPosition: '0 0, 50px 50px',
+        }}
       >
-        <div
-          className="bg-main-background flex min-h-screen flex-col gap-5 bg-repeat p-5"
-          style={{
-            backgroundImage: `url('./background/rick-head.svg'), url('./background/morty-head.svg')`,
-            backgroundPosition: '0 0, 50px 50px',
-          }}
-        >
-          <Wrapper />
-        </div>
-      </ErrorBoundary>
-    );
-  }
+        <Wrapper />
+      </div>
+    </ErrorBoundary>
+  );
 }
 
 export default App;
