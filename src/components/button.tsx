0b063f59fc2040be98eef3db6f1b23e86d36f726
Mxm Msv <oikioikioikiabrams@gmail.com>
2025-07-22 10:16:47 +0300

    refactor: change class component to functional component for button.tsx

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,25 +1,20 @@
-import { Component } from 'react';
-
 import { cn } from '@/utils/cn';
 
-class Button extends Component<React.ComponentProps<'button'>> {
-  render() {
-    const { type, children, className, ...props } = this.props;
-    return (
-      <button
-        type={type || 'button'}
-        className={cn(
-          'h-9 px-4 py-2 text-center',
-          'bg-muted-hero text-primary cursor-pointer rounded-xl shadow-xs',
-          'hover:bg-muted-hero/90 transition active:scale-95',
-          className
-        )}
-        {...props}
-      >
-        {children}
-      </button>
-    );
-  }
+function Button({ type, className, children, ...props }: React.ComponentProps<'button'>) {
+  return (
+    <button
+      type={type || 'button'}
+      className={cn(
+        'h-9 px-4 py-2 text-center',
+        'bg-muted-hero text-primary cursor-pointer rounded-xl shadow-xs',
+        'hover:bg-muted-hero/90 transition active:scale-95',
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </button>
+  );
 }
 
 export default Button;
