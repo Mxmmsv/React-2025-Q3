6763b4020d0224f5c15c42261ae60daa4e363bc7
Mxm Msv <oikioikioikiabrams@gmail.com>
2025-07-22 10:20:26 +0300

    refactor: change class component to functional component for input.tsx

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,22 +1,17 @@
-import { Component } from 'react';
-
 import { cn } from '@/utils/cn';
 
-class Input extends Component<React.ComponentProps<'input'>> {
-  render() {
-    const { type, className, ...props } = this.props;
-    return (
-      <input
-        type={type}
-        className={cn(
-          'text-primary placeholder:text-primary/30 border-muted-hero bg-transparent outline-none',
-          'flex h-9 w-full min-w-0 rounded-md border px-3 py-1 text-base shadow-xs transition md:text-sm',
-          className
-        )}
-        {...props}
-      />
-    );
-  }
+function Input({ type, className, ...props }: React.ComponentProps<'input'>) {
+  return (
+    <input
+      type={type}
+      className={cn(
+        'text-primary placeholder:text-primary/30 border-muted-hero bg-transparent outline-none',
+        'flex h-9 w-full min-w-0 rounded-md border px-3 py-1 text-base shadow-xs transition md:text-sm',
+        className
+      )}
+      {...props}
+    />
+  );
 }
 
 export default Input;
