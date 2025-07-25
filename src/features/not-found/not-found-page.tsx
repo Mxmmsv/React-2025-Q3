983bfd87154f9ab9dd09d52d962270c363e9e180
Mxm Msv <oikioikioikiabrams@gmail.com>
2025-07-25 12:33:43 +0300

    refactor: add new styles for not found page

--- src/features/not-found/not-found-page.tsx
+++ src/features/not-found/not-found-page.tsx
@@ -2,12 +2,14 @@ import { Link } from 'react-router';
 
 function NotFoundPage() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center text-center text-white">
-      <h1 className="text-5xl font-bold">404</h1>
-      <p className="mt-4 text-xl">Page not found</p>
-      <Link to="/characters" className="mt-6 text-blue-400 underline">
-        ← Go back to Characters
-      </Link>
+    <div className="flex min-h-screen flex-col items-center justify-center text-center">
+      <div className="bg-background/70 rounded-xl p-2 shadow-md">
+        <h1 className="text-5xl font-bold">404</h1>
+        <p className="mt-4 text-xl">Page not found</p>
+        <Link to="/characters" className="mt-6 text-blue-400 underline">
+          ← Go back to Characters
+        </Link>
+      </div>
     </div>
   );
 }
