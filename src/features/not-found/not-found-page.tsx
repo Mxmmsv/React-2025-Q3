ea09c5ee1e4902b614432c6363a097b9e6f59a23
Mxm Msv <oikioikioikiabrams@gmail.com>
2025-07-25 12:26:42 +0300

    feat: add not found page

---
+++ src/features/not-found/not-found-page.tsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router';
+
+function NotFoundPage() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center text-center text-white">
+      <h1 className="text-5xl font-bold">404</h1>
+      <p className="mt-4 text-xl">Page not found</p>
+      <Link to="/characters" className="mt-6 text-blue-400 underline">
+        ← Go back to Characters
+      </Link>
+    </div>
+  );
+}
+
+export default NotFoundPage;
