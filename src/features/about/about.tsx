8c878146310c30178d97b5eba2ea951d0f0a5344
Mxm Msv <oikioikioikiabrams@gmail.com>
2025-07-27 23:16:25 +0300

    style: make about beauty

--- src/features/about/about.tsx
+++ src/features/about/about.tsx
@@ -3,18 +3,29 @@ import { Link, NavLink } from 'react-router';
 function About() {
   return (
     <>
-      <NavLink to="/" className="text-accent text-2xl">
-        Home
-      </NavLink>
-      <div className="bg-muted-foreground/50 flex flex-row items-center justify-center rounded-md">
-        <div className="flex flex-col-reverse items-center">
-          <div className="flex flex-row items-center gap-5">
-            <span>RS School JS / Front-end RU Course</span>
-            <Link to={'https://rs.school/courses/javascript-ru'} target="_blank">
+      <div className="flex flex-col items-start">
+        <img
+          src="/Rick_and_Morty.svg"
+          alt="Rick and Morty logo"
+          loading="lazy"
+          decoding="async"
+          className="h-16 [filter:drop-shadow(0_0_0_var(--color-muted-hero))] transition duration-300 hover:[filter:drop-shadow(0_0_1rem_var(--color-muted-hero))]"
+        />
+        <NavLink to="/" className="cursor-pointer text-2xl text-white">
+          Home
+        </NavLink>
+      </div>
+      <div className="flex flex-row items-center justify-center gap-5 rounded-md">
+        <Link
+          to={'https://rs.school/courses/reactjs'}
+          target="_blank"
+          className="bg-background cursor-pointer rounded-xl p-2 shadow-md transition hover:scale-[1.02]"
+        >
+          <div className="flex flex-col-reverse items-center">
+            <div className="flex flex-row items-center gap-5">
+              <span>RS School / React Course</span>
               <img src="/rs-logo.svg" alt="rs-logo" width={32} height={32} />
-            </Link>
-          </div>
-          <Link to={'https://rs.school/courses/javascript-ru'} target="_blank">
+            </div>
             <img
               src="/rss-welcome.webp"
               alt="RSS welcome sticker"
@@ -22,22 +33,26 @@ function About() {
               decoding="async"
               className="rounded-md [filter:drop-shadow(0_0_0_var(--color-chart-3))] transition duration-500 hover:[filter:drop-shadow(0_0_1rem_var(--color-chart-3))]"
             />
-          </Link>
-        </div>
-        <div className="flex flex-col items-center">
-          <p>Maxim Moiseev</p>
-          <img
-            src="/Moomin.webp"
-            alt="RSS welcome sticker"
-            loading="lazy"
-            decoding="async"
-            width={300}
-            height={300}
-          />
-          <Link to={'https://github.com/Mxmmsv'} target="_blank">
+          </div>
+        </Link>
+        <Link
+          to={'https://github.com/Mxmmsv'}
+          target="_blank"
+          className="bg-background cursor-pointer rounded-xl p-2 shadow-md transition hover:scale-[1.02]"
+        >
+          <div className="flex flex-col items-center">
+            <p>Maxim Moiseev</p>
+            <img
+              src="/Moomin.webp"
+              alt="RSS welcome sticker"
+              loading="lazy"
+              decoding="async"
+              width={300}
+              height={300}
+            />
             Github
-          </Link>
-        </div>
+          </div>
+        </Link>
       </div>
     </>
   );
