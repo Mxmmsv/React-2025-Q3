e6019b59a59bc1defb2df40bba8c54a843a9c255
Mxm Msv <oikioikioikiabrams@gmail.com>
2025-07-27 19:23:30 +0300

    fix: fixed character-list test after adding router

--- src/features/main/character-list.test.tsx
+++ src/features/main/character-list.test.tsx
@@ -1,4 +1,5 @@
 import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router';
 import { it, expect } from 'vitest';
 
 import characterMock from '@/api/__mocks__/characters.mock';
@@ -6,6 +7,10 @@ import characterMock from '@/api/__mocks__/characters.mock';
 import CharacterList from './character-list';
 
 it('Should render character list', () => {
-  const { container } = render(<CharacterList character={characterMock[0]} />);
+  const { container } = render(
+    <MemoryRouter>
+      <CharacterList character={characterMock[0]} />
+    </MemoryRouter>
+  );
   expect(container).toBeInTheDocument();
 });
