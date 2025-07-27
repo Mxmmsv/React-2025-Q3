2a2bb3dfa17c10905a8c9eef0bc6c0b74c854927
Mxm Msv <oikioikioikiabrams@gmail.com>
2025-07-27 11:35:18 +0300

    refactor: change tests after mentor check

--- src/features/header/header.test.tsx
+++ src/features/header/header.test.tsx
@@ -5,26 +5,24 @@ import { describe, it, expect } from 'vitest';
 import Header from './header';
 
 describe('Header', () => {
-  describe('Render tests', () => {
-    it('Should render header', () => {
-      const { container } = render(
-        <MemoryRouter>
-          <Header />
-        </MemoryRouter>
-      );
+  it('Should render header', () => {
+    const { container } = render(
+      <MemoryRouter>
+        <Header />
+      </MemoryRouter>
+    );
 
-      expect(container).toBeInTheDocument();
-    });
+    expect(container).toBeInTheDocument();
+  });
 
-    it('Should render logo image for header', () => {
-      render(
-        <MemoryRouter>
-          <Header />
-        </MemoryRouter>
-      );
-      const logo = screen.getByRole('img');
+  it('Should render logo image for header', () => {
+    render(
+      <MemoryRouter>
+        <Header />
+      </MemoryRouter>
+    );
+    const logo = screen.getByRole('img');
 
-      expect(logo).toBeInTheDocument();
-    });
+    expect(logo).toBeInTheDocument();
   });
 });
