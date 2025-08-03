80d8c103ad0a9d4745f7290e5870dcc9eb007dd8
Mxm Msv <oikioikioikiabrams@gmail.com>
2025-08-03 22:19:56 +0300

    test: fix after install redux

--- src/features/main/character-list.test.tsx
+++ src/features/main/character-list.test.tsx
@@ -1,16 +1,20 @@
 import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
 import { MemoryRouter } from 'react-router';
 import { it, expect } from 'vitest';
 
 import characterMock from '@/api/__mocks__/characters.mock';
+import { store } from '@/services/store/store';
 
 import CharacterList from './character-list';
 
 it('Should render character list', () => {
   const { container } = render(
-    <MemoryRouter>
-      <CharacterList character={characterMock[0]} />
-    </MemoryRouter>
+    <Provider store={store}>
+      <MemoryRouter>
+        <CharacterList character={characterMock[0]} />
+      </MemoryRouter>
+    </Provider>
   );
   expect(container).toBeInTheDocument();
 });
