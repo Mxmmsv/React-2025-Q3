3428b006724588b2f2cc9ed2ad49786d6f9ba6b0
Mxm Msv <oikioikioikiabrams@gmail.com>
2025-08-03 15:27:45 +0300

    chore: install redux-react

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,22 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router';
 
 import '@/style/index.css';
 import App from '@/app/App.tsx';
+import { store } from '@/services/store/store';
 
 const rootElement = document.getElementById('root');
 
 if (!rootElement) throw new Error('Root element not found!');
 
 createRoot(rootElement).render(
-  <BrowserRouter>
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <StrictMode>
+        <App />
+      </StrictMode>
+    </BrowserRouter>
+  </Provider>
 );
